c6825632391fb830b5071f8691ab0dfa7d895283
ultimatile <kaitaiti@live.jp>
2017-03-05 09:48:54 +0900

    using itertools

--- makematrix_sparse.py
+++ makematrix_sparse.py
@@ -1,6 +1,7 @@
 #make matrix rep of hamiltonan
 import numpy as np
 from scipy.sparse import lil_matrix
+import itertools
 
 def Braket(basisseq,column,row,spin):
     braket=np.hstack((np.fliplr(basisseq[row])[spin],basisseq[column,spin]))
@@ -20,58 +21,59 @@ def Parity(sitesize,diff,braket):# fermion sign
 
 def  MatrixRep(sitesize,basisseq,basissize,t1,t2,U,V1,V2,bc):
     MR=lil_matrix((basissize,basissize))#initialization
-    for i in range(basissize):
-        for j in range(basissize):
-            Diff =basisseq[i]-basisseq[j]
-            if (Diff[0]==0).all():
-                if (Diff[1]==0).all():#diagonal elements
-                    MR[i,j]+=U*np.array(np.where(sum(basisseq[i],0)==2)).size#onsite interaction
-                    for l in range(sitesize-1):#NNinteraction(nonBoundary)
-                        MR[i,j]+=V1*sum(basisseq[i],0)[l]*sum(basisseq[i],0)[l+1]
-                    MR[i,j]+=V1*sum(basisseq[i],0)[sitesize-1]*sum(basisseq[i],0)[0]*bc*bc#NNinteraction(boundary)
-                    for m in range(sitesize-2):#NNNinteraction(nonboundary)
-                        MR[i,j]+=V2*sum(basisseq[i],0)[m]*sum(basisseq[i],0)[m+2]
-                    MR[i,j]+=V2*sum(basisseq[i],0)[sitesize-2]*sum(basisseq[i],0)[0]*bc*bc#NNNinteraction(boundary)1
-                    MR[i,j]+=V2*sum(basisseq[i],0)[sitesize-1]*sum(basisseq[i],0)[1]*bc*bc#NNNinteraction(boundary)2
-                else: #downhopping
-                    if np.array(np.nonzero(Diff[1])).size==2:
-                    #exclude many electrons hopping concurrently
-                        if abs(np.array(np.nonzero(Diff[1]))[0,0]-np.array(np.nonzero(Diff[1]))[0,1])==1:
-                            #NNhopping(NonBoundary)
-                            bk=Braket(basisseq,i,j,1)
-                            MR[i,j]+=-t1*Parity(sitesize,Diff[1],bk)
-                        elif abs(np.array(np.nonzero(Diff[1]))[0,0]-np.array(np.nonzero(Diff[1]))[0,1])==2:
-                            #NNNhopping(Nonboundary)
-                            bk=Braket(basisseq,i,j,1)
-                            MR[i,j]+=-t2*Parity(sitesize,Diff[1],bk)
-                        if abs(np.array(np.nonzero(Diff[1]))[0,0]-np.array(np.nonzero(Diff[1]))[0,1])==sitesize-1:
-                            #NNhopping(Boundary)
-                            bk=Braket(basisseq,i,j,1)
-                            MR[i,j]+=-t1*bc*Parity(sitesize,Diff[1],bk)
-                        elif abs(np.array(np.nonzero(Diff[1]))[0,0]-np.array(np.nonzero(Diff[1]))[0,1])==sitesize-2:
-                            #NNNhopping(Boundary)
-                            bk=Braket(basisseq,i,j,1)
-                            MR[i,j]+=-t2*bc*Parity(sitesize,Diff[1],bk)
-            else:
-                if (Diff[1]==0).all():#uphopping
-                    if np.array(np.nonzero(Diff[0])).size==2:
-                    #exclude many electrons hopping concurrently
-                        if abs(np.array(np.nonzero(Diff[0]))[0,0]-np.array(np.nonzero(Diff[0]))[0,1])==1:
-                            #NNhopping(NonBoundary)
-                            bk=Braket(basisseq,i,j,0)
-                            MR[i,j]+=-t1*Parity(sitesize,Diff[0],bk)
-                        elif abs(np.array(np.nonzero(Diff[0]))[0,0]-np.array(np.nonzero(Diff[0]))[0,1])==2:
-                            #NNNhopping(Nonboundary)
-                            bk=Braket(basisseq,i,j,0)
-                            MR[i,j]+=-t2*Parity(sitesize,Diff[0],bk)
-                        if abs(np.array(np.nonzero(Diff[0]))[0,0]-np.array(np.nonzero(Diff[0]))[0,1])==sitesize-1:
-                            #NNhopping(Boundary)
-                            bk=Braket(basisseq,i,j,0)
-                            MR[i,j]+=-t1*bc*Parity(sitesize,Diff[0],bk)
-                        elif abs(np.array(np.nonzero(Diff[0]))[0,0]-np.array(np.nonzero(Diff[0]))[0,1])==sitesize-2:
-                            #NNNhopping(Boundary)
-                            bk=Braket(basisseq,i,j,0)
-                            MR[i,j]+=-t2*bc*Parity(sitesize,Diff[0],bk)
+    for i, j in itertools.product(range(basissize), range(basissize)):
+    #for i in range(basissize):
+    #for j in range(basissize):
+        Diff =basisseq[i]-basisseq[j]
+        if (Diff[0]==0).all():
+            if (Diff[1]==0).all():#diagonal elements
+                MR[i,j]+=U*np.array(np.where(sum(basisseq[i],0)==2)).size#onsite interaction
+                for l in range(sitesize-1):#NNinteraction(nonBoundary)
+                    MR[i,j]+=V1*sum(basisseq[i],0)[l]*sum(basisseq[i],0)[l+1]
+                MR[i,j]+=V1*sum(basisseq[i],0)[sitesize-1]*sum(basisseq[i],0)[0]*bc*bc#NNinteraction(boundary)
+                for m in range(sitesize-2):#NNNinteraction(nonboundary)
+                    MR[i,j]+=V2*sum(basisseq[i],0)[m]*sum(basisseq[i],0)[m+2]
+                MR[i,j]+=V2*sum(basisseq[i],0)[sitesize-2]*sum(basisseq[i],0)[0]*bc*bc#NNNinteraction(boundary)1
+                MR[i,j]+=V2*sum(basisseq[i],0)[sitesize-1]*sum(basisseq[i],0)[1]*bc*bc#NNNinteraction(boundary)2
+            else: #downhopping
+                if np.array(np.nonzero(Diff[1])).size==2:
+                #exclude many electrons hopping concurrently
+                    if abs(np.array(np.nonzero(Diff[1]))[0,0]-np.array(np.nonzero(Diff[1]))[0,1])==1:
+                        #NNhopping(NonBoundary)
+                        bk=Braket(basisseq,i,j,1)
+                        MR[i,j]+=-t1*Parity(sitesize,Diff[1],bk)
+                    elif abs(np.array(np.nonzero(Diff[1]))[0,0]-np.array(np.nonzero(Diff[1]))[0,1])==2:
+                        #NNNhopping(Nonboundary)
+                        bk=Braket(basisseq,i,j,1)
+                        MR[i,j]+=-t2*Parity(sitesize,Diff[1],bk)
+                    if abs(np.array(np.nonzero(Diff[1]))[0,0]-np.array(np.nonzero(Diff[1]))[0,1])==sitesize-1:
+                        #NNhopping(Boundary)
+                        bk=Braket(basisseq,i,j,1)
+                        MR[i,j]+=-t1*bc*Parity(sitesize,Diff[1],bk)
+                    elif abs(np.array(np.nonzero(Diff[1]))[0,0]-np.array(np.nonzero(Diff[1]))[0,1])==sitesize-2:
+                        #NNNhopping(Boundary)
+                        bk=Braket(basisseq,i,j,1)
+                        MR[i,j]+=-t2*bc*Parity(sitesize,Diff[1],bk)
+        else:
+            if (Diff[1]==0).all():#uphopping
+                if np.array(np.nonzero(Diff[0])).size==2:
+                #exclude many electrons hopping concurrently
+                    if abs(np.array(np.nonzero(Diff[0]))[0,0]-np.array(np.nonzero(Diff[0]))[0,1])==1:
+                        #NNhopping(NonBoundary)
+                        bk=Braket(basisseq,i,j,0)
+                        MR[i,j]+=-t1*Parity(sitesize,Diff[0],bk)
+                    elif abs(np.array(np.nonzero(Diff[0]))[0,0]-np.array(np.nonzero(Diff[0]))[0,1])==2:
+                        #NNNhopping(Nonboundary)
+                        bk=Braket(basisseq,i,j,0)
+                        MR[i,j]+=-t2*Parity(sitesize,Diff[0],bk)
+                    if abs(np.array(np.nonzero(Diff[0]))[0,0]-np.array(np.nonzero(Diff[0]))[0,1])==sitesize-1:
+                        #NNhopping(Boundary)
+                        bk=Braket(basisseq,i,j,0)
+                        MR[i,j]+=-t1*bc*Parity(sitesize,Diff[0],bk)
+                    elif abs(np.array(np.nonzero(Diff[0]))[0,0]-np.array(np.nonzero(Diff[0]))[0,1])==sitesize-2:
+                        #NNNhopping(Boundary)
+                        bk=Braket(basisseq,i,j,0)
+                        MR[i,j]+=-t2*bc*Parity(sitesize,Diff[0],bk)
     MR=MR.tocsr()
     return MR
 
